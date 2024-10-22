a2526398e08b8f69ac14f0866baac6c281172c0f
MattM6789 <mattm6789@gmail.com>
2024-10-22 15:05:39 -0500

    Unit and Wire LWC with tests

--- force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
+++ force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
@@ -20,8 +20,8 @@ describe('c-unit-test', () => {
     const div = element.shadowRoot.querySelector('div');
     expect(div.textContent).toBe('Unit 5 alive!');
   });
-    
-      it('displays unit status with updated unitNumber', () => {
+
+  it('displays unit status with updated unitNumber', () => {
     const element = createElement('c-unit-test', {
      is: UnitTest
     });
@@ -32,6 +32,29 @@ describe('c-unit-test', () => {
       
     const div = element.shadowRoot.querySelector('div');
     // Verify displayed unit status
-    expect(div.textContent).toBe('Unit 6 alive!');
+     expect(div.textContent).not.toBe('Unit 6 alive!');
+    
+    // Return a promise to wait for any asynchronous DOM updates. Jest
+    // will automatically wait for the Promise chain to complete before
+    // ending the test and fail the test if the promise rejects.
+    return Promise.resolve().then(() => {
+      expect(div.textContent).toBe('Unit 6 alive!');
+    });
+  });
+  it('displays unit status with input change event', () => {
+    const element = createElement('c-unit-test', {
+      is: UnitTest
+    });
+    document.body.appendChild(element);
+    const div = element.shadowRoot.querySelector('div');
+      
+    // Trigger unit status input change
+    const inputElement = element.shadowRoot.querySelector('lightning-input');
+    inputElement.value = 7;
+    inputElement.dispatchEvent(new CustomEvent('change'));
+      
+    return Promise.resolve().then(() => {
+      expect(div.textContent).toBe('Unit 7 alive!');
+    });
   });
 });
