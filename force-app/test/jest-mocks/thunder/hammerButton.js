c229b69c28ea40c046f8d98413d140ddba40b5e4
MattM6789 <mattm6789@gmail.com>
2024-10-22 19:18:29 -0500

    Mock other components

---
+++ force-app/test/jest-mocks/thunder/hammerButton.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+
+export default class HammerButton extends LightningElement{
+    @api lable;
+    //any other implementation ou may want to expose here
+}
